47e1d1c28568f3d2254e9a999c502025874e2ada
Alon Tal <alontal23@gmail.com>
2020-01-20 14:03:22 +0200

    bug fixes

--- frontend/src/pages/HouseDetails.js
+++ frontend/src/pages/HouseDetails.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-import ChatBox from '../cmps/ChatBox'
-import '../assets/styles/housedetails.css'
+import { connect } from 'react-redux';
+// import ChatBox from '../cmps/ChatBox'
+import NavBar from '../cmps/NavBar'
+import '../assets/styles/housedetails.css';
 
 
 
 export default class HouseDetails extends Component {
-    house = {
+    state = {
+        house: {
             "_id": "0p9o8i",
             "title": "House on the Lake",
             "description": "Central location and nice view",
@@ -44,26 +46,48 @@ export default class HouseDetails extends Component {
                 }
             ]
         }
+    }
 
     render() {
         return (
-            <section className="housedetails-container">
-                <div className="images-container">
-                    <div className="gallery">
-                        <img key={0} className="img img-1" src={this.house.imgs[0]} alt="" />
-                        <img key={1} className="img img-2" src={this.house.imgs[1]} alt="" />
-                        <img key={2} className="img img-3" src={this.house.imgs[2]} alt="" />
-                        <img key={3} className="img img-4" src={this.house.imgs[3]} alt="" />
-                        <img key={4} className="img img-5" src={this.house.imgs[4]} alt="" />
+            <React.Fragment>
+                <NavBar />
+                <section className="housedetails-container">
+                    <div className="images-container">
+                        <div className="gallery">
+                            <img key={0} className="img img-1" src={this.house.imgs[0]} alt="" />
+                            <img key={1} className="img img-2" src={this.house.imgs[1]} alt="" />
+                            <img key={2} className="img img-3" src={this.house.imgs[2]} alt="" />
+                            <img key={3} className="img img-4" src={this.house.imgs[3]} alt="" />
+                            <img key={4} className="img img-5" src={this.house.imgs[4]} alt="" />
+                        </div>
                     </div>
-                </div>
-                {/* <ChatBox house={this.house}></ChatBox> */}
-            </section>
-
+                    {/* <ChatBox house={this.house}></ChatBox> */}
+                </section>
+            </React.Fragment>
         )
     }
 }
 
+
+
+
+
+
+
+
+
+// componentDidMount() {
+//     this.loadHouse()
+// }
+
+// loadHouse = () => {
+//     const houseId = this.props.match.params.id;
+//     houseService.get(houseId).then(car=>{
+//         this.setState({car});
+//     })
+//     console.log(houseId);
+// }
 
 // const mapStateToProps = state => {
 //     return {
@@ -71,5 +95,5 @@ export default class HouseDetails extends Component {
 //   };
 //   const mapDispatchToProps = {
 //   };
-  
+
 //   export default connect(mapStateToProps, mapDispatchToProps)(HouseDetails)
