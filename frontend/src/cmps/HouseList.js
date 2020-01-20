866248eb743883e541c7136faa29999c82cce5ee
shay rosenthal <shayke.rosenthal@gmail.com>
2020-01-20 16:59:57 +0200

    20.01.20 17:00 shay

--- frontend/src/cmps/HouseList.js
+++ frontend/src/cmps/HouseList.js
@@ -2,125 +2,9 @@ import React, { Component } from "react";
 // import { withRouter } from "react-router";
 import HousePreview from "./HousePreview";
 import NavBar from "./NavBar";
-import image1 from "../assets/img/crap1.jpg";
-import image2 from "../assets/img/crap2.jpg";
+// import image1 from "../assets/img/crap1.jpg";
+// import image2 from "../assets/img/crap2.jpg";
 class HouseList extends Component {
-<<<<<<< HEAD
-    houses = [
-        {
-            "_id": "0p9o8i",
-            "title": "House on the Lake",
-            "description": "Central location and nice view",
-            "owner": {
-                "_id": "0987",
-                "fullName": "Yael Shenker",
-                "imgURL": "https://a0.muscache.com/im/pictures/user/e23d9c03-0de7-46f9-ba5a-c62a6358ff34.jpg?aki_policy=profile_x_medium"
-            },
-            "address": {
-                "country": "Spain",
-                "coords": {
-                    "lat": 0,
-                    "lng": 0
-                }
-            },
-            "imgs": [
-                "https://a0.muscache.com/im/pictures/41671788/112cfec6_original.jpg?aki_policy=xx_large",
-                "https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?fit=scale",
-                "https://si.wsj.net/public/resources/images/B3-DM067_RIGHTS_IM_20190319162958.jpg"
-            ],
-            "capacity": 4,
-            "price": 100,
-            "reviews": [
-                {
-                    "byUser": {
-                        "_id": "1234",
-                        "fullName": "shay rosenthal",
-                        "img": "http://img"
-                    },
-                    "txt": "Amazing place",
-                    "rate": 4,
-                    "createdAt": "2020-01-15T07:51:18.138Z"
-                }
-            ]
-        },
-        {
-            "_id": "0p9o8j",
-            "title": "House on the Lake",
-            "description": "Central location and nice view",
-            "owner": {
-                "_id": "0987",
-                "fullName": "Yael Shenker",
-                "imgURL": "https://a0.muscache.com/im/pictures/user/e23d9c03-0de7-46f9-ba5a-c62a6358ff34.jpg?aki_policy=profile_x_medium"
-            },
-            "address": {
-                "country": "Spain",
-                "coords": {
-                    "lat": 0,
-                    "lng": 0
-                }
-            },
-            "imgs": [
-                "https://a0.muscache.com/im/pictures/41671788/112cfec6_original.jpg?aki_policy=xx_large",
-                "https://a0.muscache.com/im/pictures/beb041ce-a69f-4be0-988d-1305d351e3d1.jpg?aki_policy=large",
-                "https://a0.muscache.com/im/pictures/b87c9f56-bd6e-4cfc-b1af-5c122c5c5066.jpg?aki_policy=large",
-                "https://a0.muscache.com/im/pictures/a52deab7-1485-4d75-b871-b468b3f4a08d.jpg?aki_policy=large",
-                "https://a0.muscache.com/im/pictures/f3259994-cf98-4c4a-a532-c1ac95e65477.jpg?aki_policy=large"
-            ],
-            "capacity": 4,
-            "price": 100,
-            "reviews": [
-                {
-                    "byUser": {
-                        "_id": "1234",
-                        "fullName": "shay rosenthal",
-                        "img": "http://img"
-                    },
-                    "txt": "Amazing place",
-                    "rate": 4,
-                    "createdAt": "2020-01-15T07:51:18.138Z"
-                }
-            ]
-        },
-        {
-            "_id": "0p9o8k",
-            "title": "House on the Lake",
-            "description": "Central location and nice view",
-            "owner": {
-                "_id": "0987",
-                "fullName": "Yael Shenker",
-                "imgURL": "https://a0.muscache.com/im/pictures/user/e23d9c03-0de7-46f9-ba5a-c62a6358ff34.jpg?aki_policy=profile_x_medium"
-            },
-            "address": {
-                "country": "Spain",
-                "coords": {
-                    "lat": 0,
-                    "lng": 0
-                }
-            },
-            "imgs": [
-                "https://a0.muscache.com/im/pictures/41671788/112cfec6_original.jpg?aki_policy=xx_large",
-                "https://a0.muscache.com/im/pictures/beb041ce-a69f-4be0-988d-1305d351e3d1.jpg?aki_policy=large",
-                "https://a0.muscache.com/im/pictures/b87c9f56-bd6e-4cfc-b1af-5c122c5c5066.jpg?aki_policy=large",
-                "https://a0.muscache.com/im/pictures/a52deab7-1485-4d75-b871-b468b3f4a08d.jpg?aki_policy=large",
-                "https://a0.muscache.com/im/pictures/f3259994-cf98-4c4a-a532-c1ac95e65477.jpg?aki_policy=large"
-            ],
-            "capacity": 4,
-            "price": 100,
-            "reviews": [
-                {
-                    "byUser": {
-                        "_id": "1234",
-                        "fullName": "shay rosenthal",
-                        "img": "http://img"
-                    },
-                    "txt": "Amazing place",
-                    "rate": 4,
-                    "createdAt": "2020-01-15T07:51:18.138Z"
-                }
-            ]
-        }
-    ]
-=======
     // houses = [
     //     {
     //         "_id": "0p9o8i",
@@ -237,7 +121,6 @@ class HouseList extends Component {
     //         ]
     //     }
     // ]
->>>>>>> f5cc6aee98d163f9433a514cf2ccaa7959bde3d3
 
     style = { backgroundColor: 'red', position: 'fixed' }
     render() {
