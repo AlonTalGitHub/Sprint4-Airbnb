dc8a593278d3c971bbdca9c4c3c3c2f089765716
shay rosenthal <shayke.rosenthal@gmail.com>
2020-02-16 17:58:13 +0200

    shay

---
+++ backend/public/precache-manifest.aa82017c4520f07026070447a041ae65.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98628ac0f1c65b92d313dbb0a62f860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "980d41ba2b8a842291d6",
+    "url": "/static/css/2.2221c98b.chunk.css"
+  },
+  {
+    "revision": "3fe16c12a09fcf7daa0a",
+    "url": "/static/css/main.0aef2661.chunk.css"
+  },
+  {
+    "revision": "980d41ba2b8a842291d6",
+    "url": "/static/js/2.7242141a.chunk.js"
+  },
+  {
+    "revision": "82b3a8e7be5e15cb1c7d126d4f414ef8",
+    "url": "/static/js/2.7242141a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fe16c12a09fcf7daa0a",
+    "url": "/static/js/main.7f24683a.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "4b6c9fc5751246c99e2d21283ed30a56",
+    "url": "/static/media/bgc.4b6c9fc5.jpg"
+  },
+  {
+    "revision": "73da7f75399673092dc0a175083beef4",
+    "url": "/static/media/crap1.73da7f75.jpg"
+  },
+  {
+    "revision": "d80d571f642c1daab4e7ee81c3978dd7",
+    "url": "/static/media/loading.d80d571f.gif"
+  },
+  {
+    "revision": "891f6be3fe5713b88ceec4054764e150",
+    "url": "/static/media/turtle3.891f6be3.png"
+  },
+  {
+    "revision": "9546997720f829ecfa885e5f0321b160",
+    "url": "/static/media/turtle4.95469977.png"
+  },
+  {
+    "revision": "7ea4af7d8401d2b43ee841bfa2abe89d",
+    "url": "/static/media/user_prof_img.7ea4af7d.png"
+  }
+]);
